98faf975f80b0da4cf1b8aa789c9db09fc95dd59
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2024-05-30 16:22:12 +0800

    added area, bar and line charts show summary

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import DataCharts from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
 
 export default function DashBoardPage() {
@@ -5,6 +6,7 @@ export default function DashBoardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
+      <DataCharts />
     </div>
   );
 }
